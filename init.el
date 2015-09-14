f9ed9c2536155bafe4b9ba2a8077290c8f5105cf
Chigo Esperisto <esperisto@gmail.com>
2015-09-14 23:33:27 +0800

    Routine update

--- init.el
+++ init.el
@@ -12,19 +12,9 @@
 (require 'orgconfig)
 (require 'dictionary.app)
 (require 'mkdconfig)
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(require 'yasconfig)
+(require 'textsizechange)
 
-(setq ispell-program-name "aspell")
-
-
-
-
-
-;; 用于查看图片时使用触摸板横向移动
-(global-set-key [wheel-right] 'forward-char)
-(global-set-key [wheel-left] 'backward-char)
-;; 字体设置
-(set-default-font "-*-Consolas-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1") 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -33,46 +23,51 @@
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
-   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(background-color "#202020")
- '(background-mode dark)
- '(compilation-message-face (quote default))
+   ["#212121" "#CC5542" "#6aaf50" "#7d7c61" "#5180b3" "#DC8CC3" "#9b55c3" "#bdbdb3"])
  '(current-language-environment "UTF-8")
- '(cursor-color "#cccccc")
- '(custom-enabled-themes (quote (monokai)))
+ '(custom-enabled-themes (quote (solarized)))
  '(custom-safe-themes
    (quote
-    ("7a00b0710bb2e400d33a925f94b1cd8cfa2281f864ac9506b9046703e0045d66" "e587bd7ea49915da4556c1f5b535e920cb3f65f033ae636ba8ed0d2ca2a14df4" "0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" "e4e97731f52a5237f37ceb2423cb327778c7d3af7dc831788473d4a76bcc9760" "fcb62eaea3aa4b8beb35ca861c2c14e3fb2c138938590b0ed80da8cb4d73362a" "5f3234131870c0554a88368ee81efd15caae781bd4ad2971d319bdd7e2c6370c" "860f62c21cc5884dbe728471d61cf809bd1795743f5409682b48cc320202e047" "0d4cbd289f6f40534591f6ca14810926ac9a26b387d675696b6e0f77865db9b9" "b62c9bde889ff2afa916e4a0d81fe800dab81c412cc383b00e609a98ef8c3b6e" "c55b9ea93babb488baedc1db88e86cf42d6cc8f47103777dda531dc774ac1b31" "be269789a8fed701fb92ce2b4d59f7044a8f2631634a102741ca845d64a787ae" default)))
- '(default-input-method "rfc1345")
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "1db337246ebc9c083be0d728f8d20913a0f46edc0a00277746ba411c149d7fe5" "8fed5e4b89cf69107d524c4b91b4a4c35bcf1b3563d5f306608f0c48f580fdf8" default)))
+ '(default-input-method "esperanto-postfix")
  '(display-time-mode t)
- '(fci-rule-color "#49483E")
- '(foreground-color "#cccccc")
- '(highlight-changes-colors ("#FD5FF0" "#AE81FF"))
- '(highlight-tail-colors
-   (("#49483E" . 0)
-    ("#67930F" . 20)
-    ("#349B8D" . 30)
-    ("#21889B" . 50)
-    ("#968B26" . 60)
-    ("#A45E0A" . 70)
-    ("#A41F99" . 85)
-    ("#49483E" . 100)))
- '(magit-diff-use-overlays nil)
+ '(fci-rule-color "#2e2e2e")
+ '(ispell-program-name "aspell")
  '(make-backup-files nil)
- '(org-from-is-user-regexp "\\<Chigo Esperisto\\>")
- '(org-mobile-directory "/Users/chigo/快盘/mobile")
- '(org-odt-convert-processes
+ '(org-export-with-sub-superscripts (quote {}))
+ '(package-archives
    (quote
-    (("LibreOffice" "/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to %f%x --outdir %d %i")
-     ("unoconv" "unoconv -f %f -o %d %i"))))
- '(org-odt-preferred-output-format "docx")
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/"))))
+ '(package-selected-packages
+   (quote
+    (ample-zen-theme color-theme-solarized cyberpunk-theme)))
  '(show-paren-mode t)
- '(weechat-color-list
-   (unspecified "#272822" "#49483E" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
+ '(vc-annotate-background "#3b3b3b")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#dd5542")
+     (40 . "#CC5542")
+     (60 . "#fb8512")
+     (80 . "#baba36")
+     (100 . "#bdbc61")
+     (120 . "#7d7c61")
+     (140 . "#6abd50")
+     (160 . "#6aaf50")
+     (180 . "#6aa350")
+     (200 . "#6a9550")
+     (220 . "#6a8550")
+     (240 . "#6a7550")
+     (260 . "#9b55c3")
+     (280 . "#6CA0A3")
+     (300 . "#528fd1")
+     (320 . "#5180b3")
+     (340 . "#6380b3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-;; (put 'scroll-left 'disabled nil)
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "Input Sans Narrow")))))
---
+++ lisp/yasconfig.el
@@ -0,0 +1,7 @@
+(add-to-list 'load-path
+	     "~/.emacs.d/lisp/yasnippet")
+(require 'yasnippet) 
+(yas-global-mode 1) ;; or M-x yas-reload-all if you've started YASnippet already.
+
+
+(provide 'yasconfig)
